6232d343b83a350bb2bb7950214616418dad34a6
Nicole Rivera <41299759+nicolerivera1@users.noreply.github.com>
2020-11-26 14:35:23 -0500

    send for mac

---
+++ send_nico.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+import os
+import sys
+
+os.system( 'g++ -std=c++11 src/Tablero.cpp src/Serpiente.cpp src/Punto.cpp src/Juego.cpp Main.cpp -o a.out -lncurses')
+os.system( './a.out')
